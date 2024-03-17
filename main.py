132349e7f3633fae83efd676eeeccb4cdb14fd97
Yang Li <82488745+li-yang-cn@users.noreply.github.com>
2024-03-17 13:58:14 +0800

    Change comment to English

--- main.py
+++ main.py
@@ -16,8 +16,6 @@ def scan_video_files(directory, extensions=['.mov', '.mp4']):
                 video_files.append(os.path.join(root, file))
     return video_files
 
-
-
 def capture_frames(video_file, frame_count=5, temp_dir='frames'):
     """
     Capture `frame_count` equally spaced frames from `video_file` and save them in `temp_dir`.
@@ -25,35 +23,29 @@ def capture_frames(video_file, frame_count=5, temp_dir='frames'):
     if not os.path.exists(temp_dir):
         os.makedirs(temp_dir)
 
-    # 使用 OpenCV 读取视频
+    # Read video uing opencv
     cap = cv2.VideoCapture(video_file)
     if not cap.isOpened():
         raise Exception("Could not open video file")
 
-    # 获取总帧数
+    # Get the frame number
     total_frames = int(cap.get(cv2.CAP_PROP_FRAME_COUNT))
 
-    # 计算间隔
+    # Calculate the interval
     interval = max(1, total_frames // frame_count)
 
     frames = []
     for i in range(frame_count):
-        # 计算帧号
         frame_num = interval * i
-        # 设置视频位置
         cap.set(cv2.CAP_PROP_POS_FRAMES, frame_num)
-
-        # 读取帧
         ret, frame = cap.read()
         if not ret:
             break
 
-        # 保存帧
         output_path = os.path.join(temp_dir, f'{os.path.basename(video_file)}_frame_{i}.png')
         cv2.imwrite(output_path, frame)
         frames.append(output_path)
 
-    # 释放视频
     cap.release()
 
     return frames
@@ -74,9 +66,9 @@ def analyze_video(video_file):
 def main(directory):
     video_files = scan_video_files(directory)
     with open('video_lib.csv', 'w', newline='') as csvfile:
-        fieldnames = ['filename', 'directory', 'creation_time', 'result1', 'result2', 'result3', 'result4', 'result5']
+        fieldnames = ['filename', 'directory', 'creation_time', 
+                      'result1', 'result2', 'result3', 'result4', 'result5']
         writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
-
         writer.writeheader()
         for video_file in video_files:
             results_json = analyze_video(video_file)  
@@ -89,5 +81,5 @@ def main(directory):
             writer.writerow(record)
 
 if __name__ == '__main__':
-    #dir = input('Please input your video directory: ')
-    main(directory="video")
+    dir = input('Please input your video directory: ')
+    main(directory=dir)
